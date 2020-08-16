ef8cee5c114652ee94ce6a22364ca2ecc597329d
nbeshouri <nbeshouri@gmail.com>
2020-08-16 16:14:14 -0700

    Edit file

--- classification/train.py
+++ classification/train.py
@@ -34,7 +34,7 @@ def run_model_on_dataset(model, dataloader, config, optimizer=None, scheduler=No
         inputs = {
             "input_ids": batch[0],
             "attention_mask": batch[1],
-            "token_type_ids": batch[2],
+            # "token_type_ids": batch[2],
             "labels": batch[3]
         }
         outputs = model(**inputs)
