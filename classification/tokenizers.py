92c03bd9f85cba6a8020523de6a2c6349696debe
nbeshouri <nbeshouri@gmail.com>
2020-08-30 18:27:41 -0700

    Fix bug

--- classification/tokenizers.py
+++ classification/tokenizers.py
@@ -13,7 +13,7 @@ TOKENIZERS = {
 def get_tokenizer(config):
     if config.tokenizer not in TOKENIZERS:
         raise ValueError()
-    tokenizer_class = TOKENIZERS['tokenizer']
+    tokenizer_class = TOKENIZERS[config.tokenizer]
 
     if hasattr(tokenizer_class, 'from_pretrained'):
         tokenizer = tokenizer_class.from_pretrained(config.tokenizer)
